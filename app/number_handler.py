74f7b4c3411593e5997eacb4423e72d9ffa9e978
Kael Kill <kael.r.kill@gmail.com>
2020-01-20 23:36:40 -0300

    Added translation and bug fixes

--- app/number_handler.py
+++ app/number_handler.py
@@ -1,3 +1,5 @@
+from flask_babel import _
+
 class Number_Handler:
     def __init__(self, number):
         self.number = number
@@ -7,7 +9,7 @@ class Number_Handler:
         self.is_negative = False
 
         self.split_number(list(number))
-        self.number_to_text()
+        self.written_form = self.number_to_text()
 
     def split_number(self, number):
         
@@ -19,7 +21,6 @@ class Number_Handler:
         
         number = [int(num) for num in number]
             
-
         for num in range(0, len(number), 3):
             num_block = number[num:num+3]
             
@@ -28,22 +29,20 @@ class Number_Handler:
 
             num_block.reverse()
             self.blocks.append(num_block)
-        
-        # self.blocks.reverse()
-        # print(number)
 
     def number_to_text(self):
-        units = ['', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove']
-        teens = ['', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove']
-        tens  = ['', 'dez', 'vinte', 'trinta', 'quarenta', 'cinquenta', 'sessenta', 'setenta', 'oitenta', 'noventa']
-        cents = ['', 'cento', 'duzentos', 'trezentos', 'quatrocentos', 'quinhentos', 'seiscentos', 'setecentos', 'oitocentos', 'novecentos']
+        units = ['', _('um'), _('dois'), _('três'), _('quatro'), _('cinco'), _('seis'), _('sete'), _('oito'), _('nove')]
+        teens = [_('dez'), _('onze'), _('doze'), _('treze'), _('quatorze'), _('quinze'), _('dezesseis'), _('dezessete'), _('dezoito'), _('dezenove')]
+        tens  = ['', _('dez'), _('vinte'), _('trinta'), _('quarenta'), _('cinquenta'), _('sessenta'), _('setenta'), _('oitenta'), _('noventa')]
+        cents = ['', _('cento'), _('duzentos'), _('trezentos'), _('quatrocentos'), _('quinhentos'), _('seiscentos'), _('setecentos'), _('oitocentos'), _('novecentos')]
 
+        cnt = 0
         for block in self.blocks:
             pre_text = []
-            # pre_text = [cents[block[0]], tens[block[1]], units[block[2]]]
-            
             if block == [1, 0, 0]:
-                pre_text.append('cem')
+                pre_text.append(_('cem'))
+            elif self.blocks == [[0, 0, 0]]:
+                pre_text.append('zero')
             else:
                 pre_text.append(cents[block[0]]) if cents[block[0]] != '' else None
                 if block[1] == 1:
@@ -52,21 +51,23 @@ class Number_Handler:
                     pre_text.append(tens[block[1]]) if tens[block[1]] != '' else None
                     pre_text.append(units[block[2]]) if units[block[2]] != '' else None
                 
-            text = ' e '.join(pre_text)
-            text = text + ' mil' if self.blocks.index(block) == 1 else text
-            # print(text)
+            text = _(' e ').join(pre_text)
+            text = text + _(' mil') if cnt == 1 else text
             self.written_blocks.append(text) if text != '' else None
+            cnt = cnt + 1
+
         self.written_blocks.reverse()
-        self.written_form = ' e '.join(self.written_blocks)
+        self.written_form = _(' e ').join(self.written_blocks)
 
         if self.is_negative:
-            self.written_form = "menos {}".format(self.written_form)
+            self.written_form = _("menos ") + self.written_form
 
         print(self.written_form)
         return self.written_form
 
 
 if __name__ == "__main__":
-    nh = Number_Handler('-1234')
+    nh = Number_Handler('1234')
     nh = Number_Handler('100000')
     nh = Number_Handler('14015')
+    nh = Number_Handler('0')
--- app/server.py
+++ app/server.py
@@ -1,11 +1,38 @@
-from flask import Flask
+from flask import Flask, request, g
+from flask_babel import Babel, _
+import json
 
+from number_handler import Number_Handler
+ 
 app = Flask(__name__)
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    if not g.get('lang_code', None):
+        translations = ['pt', 'en']
+        g.lang_code = request.accept_languages.best_match(translations)
+    return g.lang_code
 
 @app.route('/')
-def index():
-    return 'Hello'
+def home():
+    return _("Ola")
 
+@app.route('/<lang>/<number>', methods=['GET'])
+@app.route('/<number>', methods=['GET'])
+def index(number, lang='pt'):
+    g.lang_code = lang
+    if not number.isdigit():
+        return _("Argumento invalido")
+        
+    if int(number) < -99999 or int(number) > 99999:
+        return _("Número fora do intervalo")
+
+    nh = Number_Handler(number)
+    resp = dict()
+    resp[_('extenso')] = nh.written_form
+
+    return json.dumps(resp)
 
 if __name__ == "__main__":
     app.run()
